c1d9c08968cdeb0979c10aae731b0dc8d4a44742
daniml3 <danimoral1001@gmail.com>
2020-08-28 22:46:22 +0200

    Utils: add a function for writing to files
    Signed-off-by: daniml3 <danimoral1001@gmail.com>

--- src/com/daniml3/Utils.java
+++ src/com/daniml3/Utils.java
@@ -1,5 +1,9 @@
 package com.daniml3;
 
+import java.io.*;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
 public class Utils {
     public static void print(String args) {
         System.out.print(args);
@@ -7,5 +11,15 @@ public class Utils {
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
+    }
+    public static void writeToFile(String file, String data) {
+        OutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream(new File(file));
+            outputStream.write(data.getBytes(), 0, data.length());
+            outputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
