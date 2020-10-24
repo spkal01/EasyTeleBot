a33afcb929eac96f30b1213e7e60683679266b4b
spkal01 <65872023+spkal01@users.noreply.github.com>
2020-10-24 23:49:58 +0300

    Update Commands.java

--- src/com/daniml3/Commands.java
+++ src/com/daniml3/Commands.java
@@ -9,6 +9,7 @@ public class Commands {
     }
  
     public static void build() {
-    code_to_execute;
+        Telegram.sendMessage("You sure you want yo start a build\nUser: {user}"
+                .replace("{user}",System.getProperty("user.name")))                             
     }
 }
