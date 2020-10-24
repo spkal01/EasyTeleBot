2e49bafe593239e4303d64da6b2bbffa30392532
spkal01 <65872023+spkal01@users.noreply.github.com>
2020-10-24 23:53:30 +0300

    Update Commands.java

--- src/com/daniml3/Commands.java
+++ src/com/daniml3/Commands.java
@@ -10,6 +10,24 @@ public class Commands {
  
     public static void build() {
         Telegram.sendMessage("You sure you want yo start a build\nUser: {user}"
-                .replace("{user}",System.getProperty("user.name")))                             
-    }
+        .replace("{user}",System.getProperty("user.name")))                             
+    public static void yes_no() throws IOException, JSONException, InterruptedException {
+    Telegram.sendMessage("Yes or no?");
+    while (true) {
+        // We need to run getUpdates when we want to update the last message
+        Telegram.getUpdates();
+        // Check if the last message is yes or no and send the opposite.
+        if (Telegram.lastMessage.toLowerCase().equals("yes")) {
+            Telegram.sendMessage("no");
+            // Break the infinite loop as we don't want this command to be infinite.
+            break;
+        }
+        if (Telegram.lastMessage.toLowerCase().equals("no")) {
+            Telegram.sendMessage("yes");
+            // Break the infinite loop as we don't want this command to be infinite.
+            break;
+        }
+        // Wait 1 second so Telegram.lastMessage is updated correctly
+        Thread.sleep(1000);            
+        }
 }
